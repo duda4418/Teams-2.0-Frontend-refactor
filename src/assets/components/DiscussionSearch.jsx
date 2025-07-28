2bd4dc662f0a86d49b9654779676fe90614e9494
Serban David <dudadud4418@gmail.com>
2025-07-28 09:32:10 +0300

    Add dropdown menu

--- src/assets/components/DiscussionSearch.jsx
+++ src/assets/components/DiscussionSearch.jsx
@@ -2,12 +2,29 @@ import { useState } from 'react'
 import { Flex, Text, Box, Card, Avatar } from "@radix-ui/themes";
 import { TextField } from '@radix-ui/themes';
 import { MagnifyingGlassIcon } from "@radix-ui/react-icons";
+import { EllipsisVertical } from "lucide-react"
+import { DropdownMenu } from '@radix-ui/themes';
 
 
 export function DiscussionSearch() {
     return (
         <Box maxWidth="30vw" p="3" pt="1" style={{ textAlign: 'left' }}>
-            <h2 style={{ fontFamily: '"Segoe UI", Arial, sans-serif', marginLeft: '1vh' }}>Teams 2.0</h2>
+            <Flex justify="between" align="center" gap="2">
+                <h2 style={{ fontFamily: '"Segoe UI", Arial, sans-serif', marginLeft: '1vh' }}>Teams 2.0</h2>
+                <DropdownMenu.Root>
+                    <DropdownMenu.Trigger>
+                        <EllipsisVertical />
+                    </DropdownMenu.Trigger>
+                    <DropdownMenu.Content>
+                        <DropdownMenu.Item >Settings</DropdownMenu.Item>
+                       
+                        <DropdownMenu.Separator />                       
+                        <DropdownMenu.Item color="red">
+                            Log out
+                        </DropdownMenu.Item>
+                    </DropdownMenu.Content>
+                </DropdownMenu.Root>
+            </Flex>
             <TextField.Root placeholder="Search discussions…" radius='full' size="3">
                 <TextField.Slot>
                     <MagnifyingGlassIcon height="16" width="16" />
@@ -15,4 +32,5 @@ export function DiscussionSearch() {
             </TextField.Root>
         </Box>
     );}
+    
 export default DiscussionSearch;
