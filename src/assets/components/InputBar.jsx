6bc4e694012b5008870e0577ef7cabe866735bf8
Serban David <dudadud4418@gmail.com>
2025-08-01 16:44:33 +0300

    Implement OpenAi API

--- src/assets/components/InputBar.jsx
+++ src/assets/components/InputBar.jsx
@@ -3,8 +3,10 @@ import { SendHorizontal } from "lucide-react";
 import {useState, useEffect } from "react";
 import { useWebSocket } from "./WebSocketContext";
 const API_URL = import.meta.env.VITE_API_URL;
+const OPEN_AI_ID = import.meta.env.VITE_OPEN_AI_ID;
 
 export default function InputBar({discussion, user}) {
+    
 
     const [typedMessage, setTypedMessage] = useState("");
     const { send } = useWebSocket();
@@ -13,32 +15,37 @@ export default function InputBar({discussion, user}) {
         console.log("Sending message:", typedMessage);
         console.log("Discussion ID:", discussion.id);
         console.log("User ID:", user.id);
+        
         try {
-            const response = await fetch(`${API_URL}/messages`, {
+            const isOpenAI = discussion.contacts.includes(OPEN_AI_ID); 
+            const endpoint = isOpenAI ? "openai" : "messages";
+
+            const response = await fetch(`${API_URL}/${endpoint}`, {
                 method: "POST",
                 headers: {
-                "Content-Type": "application/json",
+                    "Content-Type": "application/json",
                 },
                 body: JSON.stringify({
                     discussion_id: discussion.id,
                     user_id: user.id,
-                    value: typedMessage
+                    ...(isOpenAI
+                        ? { user_message: typedMessage }
+                        : { value: typedMessage })
                 }),
-        });
+            });
 
-        if (!response.ok) {
-            throw new Error("Send message failed");
-        }
+            if (!response.ok) {
+                throw new Error("Send message failed");
+            }
 
-        const data = await response.json();
-        setTypedMessage("");
-        send({ ...data, type: "message" });
-        console.log("Message sent:", data);
+            const data = await response.json();
+            setTypedMessage("");
+            send({ ...data, type: "message" });
+            console.log("Message sent:", data);
         } catch (error) {
-        console.error("Send message error:", error);
+            console.error("Send message error:", error);
         }
     };
-
 
     return (
         <div>
